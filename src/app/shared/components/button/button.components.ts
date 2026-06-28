7ec1e4dd6dd83dba4098ca1558123a52b613882d
NorthestIron <dimaurup@mail.ru>
2026-06-29 04:35:59 +0700

    ref: changed the button attribute

--- src/app/shared/components/button/button.components.ts
+++ src/app/shared/components/button/button.components.ts
@@ -1,10 +1,10 @@
 import { Component, input, InputSignal } from '@angular/core';
 
 @Component({
-  selector: 'button[title]',
+  selector: 'button[app-button-title]',
   templateUrl: './button.components.html',
   styleUrl: './button.components.scss',
 })
 export class ButtonComponent {
-  public title: InputSignal<string> = input('Кнопка');
+  public title: InputSignal<string> = input.required({alias: 'app-button-title'});
 }
