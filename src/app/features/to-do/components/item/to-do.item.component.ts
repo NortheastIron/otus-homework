604ed0694b228a6ad1d5bd58bdcc74698b6f4338
NorthestIron <dimaurup@mail.ru>
2026-07-14 04:22:58 +0700

    feat: save after changes + disabled for tooltip

--- src/app/features/to-do/components/item/to-do.item.component.ts
+++ src/app/features/to-do/components/item/to-do.item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal } from '@angular/core';
+import { Component, inject, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +9,7 @@ import { ButtonComponent, TooltipDirective, TYPES_BUTTON } from '@shared';
 
 import { Task } from '@features/to-do/types';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { ToDoService } from '@features/to-do/services';
 
 @Component({
     selector: 'app-to-do-item',
@@ -27,6 +28,8 @@ import { ErrorStateMatcher } from '@angular/material/core';
     styleUrl: './to-do.item.component.scss',
 })
 export class ToDoItemComponent {
+    private toDoService = inject(ToDoService);
+
     public data: InputSignal<Task> = input.required();
     public isSelected: InputSignal<boolean> = input.required();
 
@@ -67,5 +70,20 @@ export class ToDoItemComponent {
 
         this.taskNameControl.setValue(this.data().text);
         this.isEdited.set(true);
+    }
+
+    protected onSaveChanges($event: PointerEvent): void {
+        $event.stopPropagation();
+        if (!this.taskNameControl.value) {
+            return;
+        }
+
+        this.toDoService.updateTask({
+            id: this.data().id,
+            text: this.taskNameControl.value.trim(),
+            description: this.data().description,
+        });
+
+        this.isEdited.set(false);
     }
 }
